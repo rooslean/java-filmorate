6f97e812ec09cb2719913f9fbad2abe938cb9691
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-04-20 16:50:03 +0300

    feat: модели User и Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+public class Film {
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    int duration;
+}
